c809f007689af6f90f301e0eff211673fba2c6da
mh <mh@immerda.ch>
2010-02-27 15:56:10 +0100

    trailing whitespaces cleanup

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,22 +1,22 @@
 class spamassassin::base {
-  # fuzzyocr and pyzor are included here by default as well as they increase 
-  # the hit-rate 
+  # fuzzyocr and pyzor are included here by default as well as they increase
+  # the hit-rate
   if $spamassassin_dcc_enabled { require dcc }
-  if $spamassassin_razor2_enabled { require razor }    
+  if $spamassassin_razor2_enabled { require razor }
   if $spamassassin_pyzor_enabled { require pyzor }
-  if $spamassassin_fuzzyocr_enabled { 
+  if $spamassassin_fuzzyocr_enabled {
 	  include fuzzyocr
   } else {
-    file { ["/etc/spamassassin/FuzzyOcr.cf", 
-			      "/etc/spamassassin/FuzzyOcr.cf.real"] : 
+    file { ["/etc/spamassassin/FuzzyOcr.cf",
+            "/etc/spamassassin/FuzzyOcr.cf.real"] :
 		  ensure => absent;
     }
-  }    
-   
+  }
+
   package { 'spamassassin':
     ensure => installed,
   }
-    
+
   case $operatingsystem {
     debian: {$spamd_servicename = "spamassassin" }
     default: {$spamd_servicename="spamd"}
@@ -27,7 +27,7 @@ class spamassassin::base {
 	  name => $spamd_servicename,
     ensure => stopped,
     enable => false,
-    hasstatus => true, 
+    hasstatus => true,
     require => Package[spamassassin],
   }
 }
