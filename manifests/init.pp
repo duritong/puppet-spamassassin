f77b63b36c3991f6e90cb7c566b6f78e75755199
mh <mh@immerda.ch>
2010-02-27 14:48:39 +0100

    take decision variables into main class

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,31 +5,28 @@
 # this module is part of a whole bunch of modules, please have a look at the exim module
 #
 
-case $spamassassin_dcc_enabled {
-    "": { $spamassassin_dcc_enabled = "false" }
-    
-}
-
-case $spamassassin_pyzor_enabled {
-    "": { $spamassassin_pyzor_enabled = "false" }
-}
-
-case $spamassassin_razor2_enabled {
-    "": { $spamassassin_razor2_enabled = "false" }
-}
-
-case $spamassassin_fuzzyocr_enabled {
-    "": { $spamassassin_fuzzyocr_enabled = "false" }
-}
-
-
-
 class spamassassin {
-    case $operatingsystem {
-        gentoo: { include spamassassin::gentoo }
-        debian,ubuntu: { include spamassassin::debian }
-        default: { include spamassassin::base }
-    }
+  case $spamassassin_dcc_enabled {
+    "": { $spamassassin_dcc_enabled = "false" }
+  }
+
+  case $spamassassin_pyzor_enabled {
+    "": { $spamassassin_pyzor_enabled = "false" }
+  }
+
+  case $spamassassin_razor2_enabled {
+    "": { $spamassassin_razor2_enabled = "false" }
+  }
+
+  case $spamassassin_fuzzyocr_enabled {
+    "": { $spamassassin_fuzzyocr_enabled = "false" }
+  }
+
+  case $operatingsystem {
+    gentoo: { include spamassassin::gentoo }
+    debian,ubuntu: { include spamassassin::debian }
+    default: { include spamassassin::base }
+  }
 }
 
 class spamassassin::base {
