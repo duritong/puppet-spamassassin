70f1fcbef8e41281479e9fb9668194a73b3659ac
mh <mh@immerda.ch>
2015-04-07 14:34:42 +0200

    linting

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,9 @@
 # manage spamassassin stuff
 # Copyright (C) 2007 admin@immerda.ch
 # GPLv3
-# this module requires the amavisd-new module, as well the razor and the dcc module
-# this module is part of a whole bunch of modules, please have a look at the exim module
+# this module requires the amavisd-new module, as well the dcc module
+# this module is part of a whole bunch of modules,
+# please have a look at the exim module
 #
 # manages shorewall with a few extras
 class spamassassin(
@@ -11,9 +12,9 @@ class spamassassin(
   $use_shorewall  = false,
 ) {
   case $::operatingsystem {
-    'Debian','Ubuntu': { include spamassassin::debian }
-    'CentOS': { include spamassassin::centos }
-    default: { include spamassassin::base }
+    'Debian','Ubuntu': { include ::spamassassin::debian }
+    'CentOS': { include ::spamassassin::centos }
+    default: { include ::spamassassin::base }
   }
   if $use_shorewall {
     include ::shorewall::rules::out::razor
