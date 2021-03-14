394d590f23c46104f857cd88b055690083d87474
mh <mh@immerda.ch>
2021-03-14 17:44:44 +0100

    linting

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -2,13 +2,13 @@
 class spamassassin::centos inherits spamassassin::base {
   require gpg
 
-  Concat['/etc/spamassassin/local.cf']{
+  Concat['/etc/spamassassin/local.cf'] {
     path => '/etc/mail/spamassassin/local.cf'
   }
 
-  file{
+  file {
     '/etc/cron.d/sa-update':
-      source  => "puppet:///modules/spamassassin/${::operatingsystem}/sa-update.cron",
+      source  => "puppet:///modules/spamassassin/${facts['os']['name']}/sa-update.cron",
       require => Package['spamassassin'],
       owner   => root,
       group   => 0,
